7788b43f0c6dd98aedbb62e7172fccad832fc585
Eke <ekalu2@unb.ca>
2022-10-15 11:46:29 -0700

    tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *main-executable section of code
+ * main- entry point of the program
  *
  * return:returns zero if no error is found
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 
 }
