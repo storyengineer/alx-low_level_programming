24e73d0e18cb3f9177abcd83331e1250e1d3df5c
Eke <ekalu2@unb.ca>
2022-10-15 12:08:31 -0700

    hatel2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,6 @@
  * Return: zero if no error, non-zero value if errors.                    
  */                                                                            
 int main(void)
-{                                                                                                                               
-	        printf("With prper grammar, but the outcme is a piece of art,\n");                                                           
-
-		        return (0);                                                                                                             
-
-			                                                                                                                                
-
-}    
+{                                                                 
+       	printf("With proper grammar, but the outcome is a piece of art,\n");            return (0);
+}	
