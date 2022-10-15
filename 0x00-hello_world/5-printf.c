3931d5447489a079d678c102c90237ad7afd34a4
Eke <ekalu2@unb.ca>
2022-10-15 12:04:52 -0700

    hatel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,9 @@
-#include <stdio.h>                                                                                                              
-
-/**                                                                                                                             
- *
- *  * main- entry point of the program                                                                                             
- *
- *   *                                                                                                                              
- *
- *    * Return: zero if no error, non-zero value if errors.                                                                          
- *
- *     */                                                                                                                             
-
-int main(void)                                                                                                                  
-
+#include <stdio.h>                                                              
+/**                                                                             
+ * main- entry point of the program                                         
+ * Return: zero if no error, non-zero value if errors.                    
+ */                                                                            
+int main(void)
 {                                                                                                                               
 	        printf("With prper grammar, but the outcme is a piece of art,\n");                                                           
 
