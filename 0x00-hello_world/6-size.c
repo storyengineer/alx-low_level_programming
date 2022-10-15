efe82a25266969e3f1e4e3c5816724f2254f5714
Eke <ekalu2@unb.ca>
2022-10-15 14:10:38 -0700

    blud

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)\n", sizeof(char));
-printf("Size of an int: %i byte(s)\n", sizeof(int));
-print("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %lld byte(s)\n",sizeof(long long int));
-printf("Size of a float: %f byte(s)\n", sizeof(float));
-return (0);
+     printf("Size of a char: %ld byte(s)\n", sizeof(char));
+     printf("Size of an int: %ld byte(s)\n", sizeof(int));
+     print("Size of a long int: %ld byte(s)\n", sizeof(long int));
+     printf("Size of a long long int: %ld byte(s)\n",sizeof(long long int));
+     printf("Size of a float: %ld byte(s)\n", sizeof(float));
+     return (0);
 }
