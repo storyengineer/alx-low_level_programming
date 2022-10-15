18ad706f2a0b7bcbbde4543a0dc9129b4683bac1
Eke <ekalu2@unb.ca>
2022-10-15 12:01:37 -0700

    newest

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>                                                                                                              
+
+/**                                                                                                                             
+ *
+ *  * main- entry point of the program                                                                                             
+ *
+ *   *                                                                                                                              
+ *
+ *    * Return: zero if no error, non-zero value if errors.                                                                          
+ *
+ *     */                                                                                                                             
+
+int main(void)                                                                                                                  
+
+{                                                                                                                               
+	        printf("With prper grammar, but the outcme is a piece of art,\n");                                                           
+
+		        return (0);                                                                                                             
+
+			                                                                                                                                
+
+}    
