a57cf573803bbfc2c9ff472b6b42ee9b132739b2
Eke <ekalu2@unb.ca>
2022-10-17 16:52:05 -0700

    sadb2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0) 
 {
-printf("n is posotive");
+printf("n is posotive\n");
 }
-else if (n = 0)
+else if (n == 0)
 {
-printf("n is zero");
+printf("n is zero\n");
 }
 else (n < 0)
 {
