fd4a01896bad7aca4ebf85955f3eb7e23ef0b64e
Eke <ekalu2@unb.ca>
2022-10-17 19:37:51 -0700

    lop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main- entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+char tuta = 'a';
+char tutb = 'z';
+
+char ch = tuta;
+while(ch == 'q' && ch == 'e')
+{
+	putchar(ch);
+	ch++;
+}
+return (0);
+}
