0ca29f2dd8c9853544ff949fc945e5c07b5c0304
Eke <ekalu2@unb.ca>
2022-10-17 19:00:22 -0700

    senjr2234

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main: entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+int tuta, tutb;
+for (tuta = 'A'; tuta <= 'Z' tuta++)
+{
+	putchar(tuta);
+	putchar('\n');
+}
+for (tutb = 'a'l tutb <= 'z' tutb++)
+{
+	putchar(tutb);
+	putchar('\n');
+}
+}
