5de5b92215fd6e57aa89de83fd5faa30058a40b1
Eke <ekalu2@unb.ca>
2022-10-17 18:06:37 -0700

    s2n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 /*
- * main: determines if n is positive, negative or equal to 0
+ ** main: Prints a random number and determines if n is positive, negative or equal to 0
  * Return: always zero
  */
 int main(void)
